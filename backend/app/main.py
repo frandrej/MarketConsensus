e78673260e38ab5b6e570c7a7cc525339f130724
AndrejFrizler <af3620@columbia.edu>
2026-01-17 15:41:49 -0500

    Update CORS middleware allowed origins

--- backend/app/main.py
+++ backend/app/main.py
@@ -47,10 +47,10 @@ app = FastAPI(
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-    "http://localhost:3000",
-    "http://127.0.0.1:3000",
-    "http://localhost:5500",      # Add this for Live Server
-    "https://impliedmarkets.onrender.com",    
+    "https://impliedmarkets.com",           
+    "https://www.impliedmarkets.com",       
+    "https://impliedmarkets.onrender.com",  
+    "http://localhost:3000",                
     "http://127.0.0.1:5500"],  
     allow_credentials=True,
     allow_methods=["*"],
